632d2af8c8622c4d059fa38a5586a64e5622179d
doughu <2313443705@qq,com>
2024-05-15 16:42:48 +0800

    update

--- src/main/java/com/simple/friends/common/ErrorCode.java
+++ src/main/java/com/simple/friends/common/ErrorCode.java
@@ -1,6 +1,5 @@
 package com.simple.friends.common;
 
-// 讲师 【coder_鱼_皮】 https://space.bilibili.com/12890453/
 
 /**
  * 错误码
@@ -41,7 +40,6 @@ public enum ErrorCode {
         return message;
     }
 
-    // https://t.zsxq.com/0emozsIJh
 
     public String getDescription() {
         return description;
